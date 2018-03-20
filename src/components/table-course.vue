8f28f64fea0eac0ca90a2324bdb381cd3c4a6b8e
廖星 <xing.liao@wutnews.net>
2018-03-20 12:53:00 +0800

    优化代码结构

--- src/components/table-course.vue
+++ src/components/table-course.vue
@@ -26,28 +26,48 @@
       }
     }
   }
+
+  window.addCourse = function (course) {
+    let $modal = document.querySelector('.layui-m-layer');
+    $modal.style.display = 'none';
+    f7.confirm(`是否将 <b>${course.name}</b> 添加到你的个人课表中？`, () => {
+      f7.showIndicator();
+      $modal.remove();
+      document.querySelector('.view-main').style.filter = '';
+      axios.post('/table/course/add', { course }).then(() => {
+        f7.hideIndicator();
+        let message = '已将该课程添加到你的个人课表中';
+        if (typeof token !== 'undefined' && token.showNativeToast) token.showNativeToast(message, true);
+        else f7.alert(message);
+      }).catch(() => {
+        f7.alert('网络请求错误');
+      });
+    }, () => {
+      $modal.style.display = 'block';
+    });
+  }
 </script>
 
 <style scoped>
-    .course-title {
-        font-size: 0.85rem;
-        color: #bbb;
-        margin: 0.5rem 0;
-    }
+  .course-title {
+    font-size: 0.85rem;
+    color: #bbb;
+    margin: 0.5rem 0;
+  }
 
-    .course-content, .course-footer {
-        font-size: 1.1rem;
-        color: #000;
-        margin: 0.5rem 0;
-    }
+  .course-content, .course-footer {
+    font-size: 1.1rem;
+    color: #000;
+    margin: 0.5rem 0;
+  }
 
-    .course-footer {
-        text-align: center;
-        margin: 2rem 0.5rem -0.3rem;
-    }
+  .course-footer {
+    text-align: center;
+    margin: 2rem 0.5rem -0.3rem;
+  }
 
-    .course-divide {
-        margin: 0;
-        line-height: 0.3rem !important;
-    }
+  .course-divide {
+    margin: 0;
+    line-height: 0.3rem !important;
+  }
 </style>
