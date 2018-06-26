56334d9ea063c5a9dc822cecf68639e895f66016
廖星 <xing.liao@wutnews.net>
2018-06-26 23:16:20 +0800

    更新学院代码

--- src/pages/college.vue
+++ src/pages/college.vue
@@ -2,31 +2,33 @@
 	<f7-page>
 		<f7-block-title>请选择学院</f7-block-title>
 		<f7-list>
-			<f7-list-item link="/college/A01" title="材料科学与工程学院"></f7-list-item>
-			<f7-list-item link="/college/A02" title="交通学院"></f7-list-item>
-			<f7-list-item link="/college/A03" title="管理学院"></f7-list-item>
-			<f7-list-item link="/college/A04" title="机电工程学院"></f7-list-item>
-			<f7-list-item link="/college/A05" title="能源与动力工程学院"></f7-list-item>
-			<f7-list-item link="/college/A06" title="土木工程与建筑学院"></f7-list-item>
-			<f7-list-item link="/college/A07" title="汽车工程学院"></f7-list-item>
-			<f7-list-item link="/college/A08" title="资源与环境工程学院"></f7-list-item>
-			<f7-list-item link="/college/A09" title="信息工程学院"></f7-list-item>
-			<f7-list-item link="/college/A10" title="计算机科学与技术学院"></f7-list-item>
-			<f7-list-item link="/college/A11" title="自动化学院"></f7-list-item>
-			<f7-list-item link="/college/A12" title="航运学院"></f7-list-item>
-			<f7-list-item link="/college/A13" title="物流工程学院"></f7-list-item>
-			<f7-list-item link="/college/A14" title="理学院"></f7-list-item>
-			<f7-list-item link="/college/A15" title="化学化工与生命科学学院"></f7-list-item>
-			<f7-list-item link="/college/A16" title="经济学院"></f7-list-item>
-			<f7-list-item link="/college/A17" title="艺术与设计学院"></f7-list-item>
-			<f7-list-item link="/college/A18" title="外国语学院"></f7-list-item>
-			<f7-list-item link="/college/A19" title="文法学院"></f7-list-item>
-			<f7-list-item link="/college/A20" title="政治与行政学院"></f7-list-item>
-			<f7-list-item link="/college/A21" title="马克思主义学院"></f7-list-item>
-			<f7-list-item link="/college/A22" title="国际教育学院"></f7-list-item>
-			<f7-list-item link="/college/A23" title="创业学院"></f7-list-item>
-			<f7-list-item link="/college/A24" title="体育部"></f7-list-item>
-			<f7-list-item link="/college/D06" title="教育科学研究院"></f7-list-item>
+			<f7-list-item link="/college/A001" title="材料科学与工程学院"></f7-list-item>
+			<f7-list-item link="/college/A002" title="材料科学与工程国际化示范学院"></f7-list-item>
+			<f7-list-item link="/college/A003" title="交通学院"></f7-list-item>
+			<f7-list-item link="/college/A016" title="管理学院"></f7-list-item>
+			<f7-list-item link="/college/A005" title="机电工程学院"></f7-list-item>
+			<f7-list-item link="/college/A006" title="能源与动力工程学院"></f7-list-item>
+			<f7-list-item link="/college/A007" title="土木工程与建筑学院"></f7-list-item>
+			<f7-list-item link="/college/A004" title="汽车工程学院"></f7-list-item>
+			<f7-list-item link="/college/A008" title="资源与环境工程学院"></f7-list-item>
+			<f7-list-item link="/college/A009" title="信息工程学院"></f7-list-item>
+			<f7-list-item link="/college/A010" title="计算机科学与技术学院"></f7-list-item>
+			<f7-list-item link="/college/A011" title="自动化学院"></f7-list-item>
+			<f7-list-item link="/college/A012" title="航运学院"></f7-list-item>
+			<f7-list-item link="/college/A013" title="物流工程学院"></f7-list-item>
+			<f7-list-item link="/college/A014" title="理学院"></f7-list-item>
+			<f7-list-item link="/college/A015" title="化学化工与生命科学学院"></f7-list-item>
+			<f7-list-item link="/college/A017" title="经济学院"></f7-list-item>
+			<f7-list-item link="/college/A018" title="艺术与设计学院"></f7-list-item>
+			<f7-list-item link="/college/A019" title="外国语学院"></f7-list-item>
+			<f7-list-item link="/college/A021" title="文法学院"></f7-list-item>
+			<f7-list-item link="/college/A022" title="政治与行政学院"></f7-list-item>
+			<f7-list-item link="/college/A020" title="马克思主义学院"></f7-list-item>
+			<f7-list-item link="/college/A023" title="国际教育学院"></f7-list-item>
+			<f7-list-item link="/college/A024" title="创业学院"></f7-list-item>
+			<f7-list-item link="/college/A025" title="体育部"></f7-list-item>
+			<f7-list-item link="/college/E006" title="教育科学研究院"></f7-list-item>
+			<f7-list-item link="/college/E007" title="继续教育学院"></f7-list-item>
 		</f7-list>
 	</f7-page>
 </template>
